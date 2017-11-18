592bbd5e6c26da534877cc7f6c0bbf87c2018dbd
Devon JohnBaptiste <devondevon@outlook.com>
2017-11-18 13:49:33 -0500

    Changed db name

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/facesafe', {
+  process.env.DATABASE_URL || 'postgres://localhost:5432/sous', {
     logging: false,
   },
 );
