b2c3072ef2fcc156d7d86ae04045ea363ec3797e
Bderiel <bderiel@gmail.com>
2017-11-17 20:36:54 -0500

    feat(models for user and fridgeItems)

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,57 +1,61 @@
-const crypto = require('crypto')
-const Sequelize = require('sequelize')
-const db = require('../db')
+const crypto = require('crypto');
+const Sequelize = require('sequelize');
+const db = require('../db');
 
 const User = db.define('user', {
   email: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
   },
   password: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   salt: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   googleId: {
-    type: Sequelize.STRING
-  }
-})
+    type: Sequelize.STRING,
+  },
+});
 
-module.exports = User
+module.exports = User;
 
 /**
  * instanceMethods
  */
 User.prototype.correctPassword = function (candidatePwd) {
-  return User.encryptPassword(candidatePwd, this.salt) === this.password
-}
+  return User.encryptPassword(candidatePwd, this.salt) === this.password;
+};
 
 /**
  * classMethods
  */
 User.generateSalt = function () {
-  return crypto.randomBytes(16).toString('base64')
-}
+  return crypto.randomBytes(16).toString('base64');
+};
 
 User.encryptPassword = function (plainText, salt) {
   return crypto
     .createHash('RSA-SHA256')
     .update(plainText)
     .update(salt)
-    .digest('hex')
-}
+    .digest('hex');
+};
 
 /**
  * hooks
  */
-const setSaltAndPassword = user => {
+const setSaltAndPassword = (user) => {
   if (user.changed('password')) {
-    user.salt = User.generateSalt()
-    user.password = User.encryptPassword(user.password, user.salt)
+    user.salt = User.generateSalt();
+    user.password = User.encryptPassword(user.password, user.salt);
   }
-}
+};
 
-User.beforeCreate(setSaltAndPassword)
-User.beforeUpdate(setSaltAndPassword)
+User.beforeCreate(setSaltAndPassword);
+User.beforeUpdate(setSaltAndPassword);
