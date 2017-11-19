c3952b19158b73b640c143e0167ca31423a72b39
Bderiel <bderiel@gmail.com>
2017-11-18 19:58:22 -0500

    feat(amazon and goog button style)

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import FontAwesome from 'react-fontawesome';
 
 const Navbar = () => (
   <nav className="navbar navbar-expand-md bg-primary navbar-dark">
     <div className="container">
-      <a className="navbar-brand" href="#"><i className="fa d-inline fa-lg fa-cloud" /><b>Sous</b></a>
+      <a className="navbar-brand" href="#"><i className="fa d-inline fa-lg fa-cloud" /><b> Sous</b></a>
       <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span className="navbar-toggler-icon" /> </button>
       <div className="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <FontAwesome name="cutlery" />
-            <a className="nav-link" href="#">Recipes</a>
+            
+            <a className="nav-link" href="#"><i className="fa fa-cutlery" aria-hidden="true" /> My Recipes</a>
           </li>
         </ul>
-        <a className="btn navbar-btn ml-2 text-white btn-secondary"><i className="fa d-inline fa-lg fa-user-circle-o" /> Sign in</a>
+        <a className="btn navbar-btn ml-2 text-white btn-secondary"><i className="fa fa-sign-in" aria-hidden="true" /> Sign in</a>
       </div>
     </div>
   </nav>
