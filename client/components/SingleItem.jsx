603fd811835319b19557c521c26b60c1dae8cc4e
Yuchen Chen <yuchchen1314@gmail.com>
2017-11-19 16:35:55 -0500

    feature single product

--- client/components/SingleItem.jsx
+++ client/components/SingleItem.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+const singleProduct = 'SingleProduct'
+const recipes = ['cook', 'fried', 'grill' ]
+const nutritionList = ['vitamin A', 'potassium', 'magnessium' ]
 
-function SingleItem(){
+function SingleItem(props){
+ 
     return (
         <div>
-            
+            <div>{singleProduct}</div>
+            <div>
+            {recipes.length&&recipes.map(recipe => {
+                return <div>{recipe}</div>
+            })}
+            </div>
+            <div>
+            {nutritionList}
+            </div>
         </div>
     )
 }
+
+export default SingleItem
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,15 @@
 const User = require('./user');
 const FridgeItems = require('./FridgeItems');
 const Fridge = require('./Fridge')
-const Test = require('./test')
 
 
-// FridgeItems.belongsToMany(User, { through: Fridge });
-Test.belongsTo(User);
-Test.belongsToMany(FridgeItems, { through: Fridge });
+
+FridgeItems.belongsToMany(User, { through: Fridge });
+
 
 
 module.exports = {
   User,
   FridgeItems,
   Fridge,
-  Test,
 };
