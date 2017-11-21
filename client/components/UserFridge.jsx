4f0222a39c79fc69ce7b07f7672db722811454d8
Bderiel <bderiel@gmail.com>
2017-11-21 12:21:05 -0500

    feat(component userFridge mounts real data from backend)

--- client/components/UserFridge.jsx
+++ client/components/UserFridge.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import SingleItem from './SingleItem';
 
 const food = [{
   name: 'Banana',
@@ -21,25 +20,40 @@ const food = [{
   calories: 150,
   image: 'https://3.imimg.com/data3/RF/DJ/MY-3966004/w-320-cashew-250x250.jpg',
 }];
-function UserFridge() {
-  return (
-    <div className="py-5">
-      <h2 className="center">Devon's Fridge</h2>
-      <div className="container flexContainer">
-        {food.length && food.map(item => (
-          <div className="fridge-items" key={item.name}>
-            <NavLink to={`/singleItem/${item.id}`} >
-              <p>{item.name}</p>
-              <img src={item.image} alt="yuchen's fault" />
-              <p>{item.calories}</p>
-            </NavLink>
-          </div>      
-        ))}
+class UserFridge extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+   // this.props.grabFridge();
+  }
+  render() {
+    console.log(this.props);
+    return (
+      <div className="py-5">
+        <h2 className="center">Devon's Fridge</h2>
+        <div className="container flexContainer">
+          {this.props.fridge.length && this.props.fridge.map(item => (
+            <div className="fridge-items" key={item.fridgeItem.name}>
+              <NavLink to={`/singleItem/${item.id}`} >
+                <p>{item.fridgeItem.name}</p>
+                <img src={item.fridgeItem.image} alt="yuchen's fault" />
+              </NavLink>
+            </div>
+          ))}
+        </div>
+        <NavLink to="/addItem">Add An Item</NavLink>
       </div>
-      <NavLink to="/addItem">Add An Item</NavLink>
-    </div>
-  );
+    );
+  }
 }
 
-export default UserFridge;
+const mapState = ({ fridge }) => ({ fridge });
+// const mapDispatch = { fetchProducts };
 
+const mapDispatchtoProps = dispatch => ({
+  grabFridge: () => {
+    dispatch(fetchProducts());
+  },
+});
+export default connect(mapState, mapDispatchtoProps)(UserFridge);
