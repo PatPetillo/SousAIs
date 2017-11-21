9773a1792d24074829284bb46f89d04787bc701d
Bderiel <bderiel@gmail.com>
2017-11-21 17:08:22 -0500

    feat(recipe model)

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,13 +1,16 @@
 const User = require('./user');
 const FridgeItems = require('./FridgeItems');
 const Fridge = require('./Fridge');
+const Recipe = require('./recipe');
 
 Fridge.belongsTo(User);
 Fridge.belongsTo(FridgeItems);
+Recipe.belongsToMany(User);
 
 
 module.exports = {
   User,
   FridgeItems,
   Fridge,
+  Recipe,
 };
