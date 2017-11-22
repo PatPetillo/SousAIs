c8457302a1ece7c5a25b782c44070fefd90a1249
Pat <patpetillo@gmail.com>
2017-11-22 11:53:24 -0500

    models updated and cleaned up --please reseed database

--- server/db/models/Fridge.js
+++ server/db/models/Fridge.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
+// quantity is a weight in ounces or grams
 const Fridge = db.define('fridge', {
   quantity: Sequelize.FLOAT,
 });
