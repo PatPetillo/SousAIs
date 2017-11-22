552373ce730f21f4e15fbbb55a6196248981d701
Bderiel <bderiel@gmail.com>
2017-11-21 21:22:42 -0500

    feat(monster route works)

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,16 +1,20 @@
 const User = require('./user');
+const db = require('../db');
 const FridgeItems = require('./FridgeItems');
 const Fridge = require('./Fridge');
 const Recipe = require('./recipe');
 
+const RecipeUser = db.define('RecipeUser', {});
+
 Fridge.belongsTo(User);
 Fridge.belongsTo(FridgeItems);
-Recipe.belongsToMany(User);
-
+Recipe.belongsToMany(User, { through: RecipeUser });
+User.belongsToMany(Recipe, {through: RecipeUser });
 
 module.exports = {
   User,
   FridgeItems,
   Fridge,
   Recipe,
+  RecipeUser,
 };
