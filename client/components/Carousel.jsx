6e91ea63c8bf970280840bbbca80638edd668732
Devon JohnBaptiste <devondevon@outlook.com>
2017-11-24 12:52:32 -0500

    Changed: pics, styles. refactored components

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 
 export default function WelcomeScreenCarousel() {
-  return(
-    <div className="carousel-container">
-    <Carousel autoPlay infiniteLoop showThumbs={false} showStatus={false} interval={3000}>
-      <div>
-        <img  className="carousel-img" src ="https://static01.nyt.com/images/2016/12/08/dining/08COOKING-SPICY-PEANUT-STEW2/08COOKING-SPICY-PEANUT-STEW2-videoSixteenByNineJumbo1600.jpg" />
-      </div>
-      <div>
-        <img  className="carousel-img" src ="https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2017/09/spaghetti-bolognese.jpg?itok=KzT6QRKe" />
-      </div>
-      <div>
-        <img  className="carousel-img" src ="http://whatsgabycooking.com/wp-content/uploads/Fatoush.jpg" />
-      </div>
-    </Carousel>
+  return (
+    <div className="carousel">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showThumbs={false}
+        showStatus={false}
+        interval={3000}
+        showArrows={false}
+      >
+        <img className="carousel-img" src="/imgs/carousel-1.jpeg" alt="" />
+        <img className="carousel-img" src="imgs/carousel-2.jpeg" alt="" />
+        <img className="carousel-img" src="imgs/carousel-3.jpeg" alt="" />
+        <img className="carousel-img" src="imgs/carousel-4.jpeg" alt="" />
+      </Carousel>
     </div>
-  )
+  );
 }
+
