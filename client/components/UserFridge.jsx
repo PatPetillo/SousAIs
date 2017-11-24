eae9c8f97ce1ae0a2c4c60239f293e010329410d
Devon JohnBaptiste <devondevon@outlook.com>
2017-11-24 12:52:32 -0500

    Changed: pics, styles. refactored components

--- client/components/UserFridge.jsx
+++ client/components/UserFridge.jsx
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
-const UserFridge = (props) => {
-  const { fridge, user } = props;
-  return (
-    <div className="py-2 page-content">
-      <div className="center">
-        <h2> {`${user.name}'s Fridge`} </h2>
-        <NavLink to="/addItem">Add An Item</NavLink>
+class UserFridge extends Component {
+  componentDidMount() {
+    document.querySelector('body').classList.add('fridge');
+  }
+  componentWillUnmount() {
+    document.querySelector('body').classList.remove('fridge');
+  }
+  render() {
+    const { fridge, user } = this.props;
+    return (
+      <div className="page-content">
+        <div className="center">
+          <h2> {`${user.name}'s Fridge`} </h2>
+          <NavLink to="/addItem" className="btn btn-primary">Add An Item</NavLink>
+        </div>
+        <div className="container flex-container">
+          {fridge.length && fridge.map(item => (
+            <NavLink to={`/singleItem/${item.id}`} key={item.name}>
+              <div className="fridge-items">
+                <p>{item.name.toUpperCase()}</p>
+                <img src={item.image} alt="yuchen's fault" />
+              </div>
+            </NavLink>
+          ))}
+        </div>
       </div>
-      <div className="container flex-container">
-        {fridge.length && fridge.map(item => (
-          <NavLink to={`/singleItem/${item.id}`} key={item.name}>
-            <div className="fridge-items">
-              <p>{item.name.toUpperCase()}</p>
-              <img src={item.image} alt="yuchen's fault" />
-            </div>
-          </NavLink>
-        ))}
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 
 const mapState = ({ fridge, user }) => ({ fridge, user });
