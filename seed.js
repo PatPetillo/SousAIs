0f45157ec44f2dce25f2434aef93d3e61ce7e6fb
Pat <patpetillo@gmail.com>
2017-11-25 13:12:02 -0500

    seed file fix

--- seed.js
+++ seed.js
@@ -1,52 +1,291 @@
 const db = require('./server/db/db');
 const User = require('./server/db/models/user');
-const FridgeItems = require('./server/db/models/FridgeItems');
-const Faker = require('faker');
+const FridgeItems = require('./server/db/models/fridgeItems');
+const Fridge = require('./server/db/models/fridge');
 
-function users() {
-  Faker.seed(124);
-  const usersProm = [];
-  for (let i = 0; i < 2; i++) {
-    usersProm.push(User.create({
-      email: Faker.internet.email(),
-      name: Faker.name.findName(),
-      password: '1',
-      salt: '1',
-      googleId: Faker.internet.userName(),
-    }));
-  }
-  return usersProm;
-}
+User.belongsToMany(FridgeItems, { through: Fridge });
+FridgeItems.belongsToMany(User, { through: Fridge });
+// User.belongsToMany(Recipe, { through: RecipeUser });
+// Recipe.belongsToMany(User, { through: RecipeUser });
 
-function product() {
-  Faker.seed(124);
-  const products = [];
-  for (let i = 1; i < 16; i++) {
-    products.push(FridgeItems.create({
-      name: Faker.commerce.productName(),
-      image: 'https://media.istockphoto.com/photos/red-apple-with-water-drops-isolated-picture-id495870175?k=6&m=495870175&s=612x612&w=0&h=fc6JeWQHsvckh-KtRqOA9h5UWyELl9Bhyn7pdnfJc9k=',
-    }));
-  }
-  return products;
-}
+const users = [
+  {
+    email: 'PatPetillo@gmail.com',
+    name: 'Pat',
+    password: 'javascriptisfun',
+    salt: '1',
+    googleId: 'PatP',
+  },
+  {
+    email: 'Bderiel@gmail.com',
+    name: 'Brian',
+    password: 'nodeisfun',
+    salt: '2',
+    googleId: 'BrianD',
+  },
+  {
+    email: 'DevonJohnBaptiste@gmail.com',
+    name: 'Devon',
+    password: 'expressisfun',
+    salt: '3',
+    googleId: 'DevonJ',
+  },
+  {
+    email: 'WaleJegede@gmail.com',
+    name: 'Wale',
+    password: 'reactisfun',
+    salt: '4',
+    googleId: 'WaleJ',
+  },
+  {
+    email: 'Yuchen@gmail.com',
+    name: 'Yuchen',
+    password: '111',
+    salt: '5',
+    googleId: 'YuchenC',
+  },
+  {
+    email: '1@gmail.com',
+    name: 'test',
+    password: '1',
+    salt: '6',
+    googleId: 'test',
+  },
 
+];
 
-db
-  .sync({ force: true })
-  .then(() => Promise.all(users()))
-  .then(() => Promise.all(product()))
-  .then(() => {
-  })
-  .then(
-    () => {
-      console.log('Seeding successful');
-    },
-    (err) => {
-      console.error('Error while seeding');
-      console.error(err.stack);
-    },
-  )
-  .finally(() => {
-    db.close();
-    return null;
-  });
+const fridgeItems = [
+  {
+    name: 'potato',
+    image: 'https://cdn.shopify.com/s/files/1/1017/2183/t/2/assets/live-preview-potato.png?17662155388061927543',
+  },
+  {
+    name: 'rice',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Rice_p1160004.jpg/200px-Rice_p1160004.jpg',
+  },
+  {
+    name: 'beef',
+    image: 'https://www.maangchi.com/wp-content/uploads/2013/09/beef.png',
+  },
+  {
+    name: 'bell pepper',
+    image: 'https://i5.walmartimages.com/asr/08463912-1cb6-446c-99b0-b245be980d48_1.a54c343d736202d4f5f3208d3d6ade6b.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF',
+  },
+  {
+    name: 'onion',
+    image: 'http://cdn.shopify.com/s/files/1/1537/5553/products/00613_15abd93a-e239-45df-acdb-8485b40d546a_grande.jpg?v=1486440965',
+  },
+  {
+    name: 'carrots',
+    image: 'http://media.mercola.com/assets/images/foodfacts/carrot-nutrition-facts.jpg',
+  },
+  {
+    name: 'soy sauce',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/63/Soy_sauce_2.jpg',
+  },
+];
+
+const fridges = [
+  {
+    quantity: 1.0,
+    userId: 1,
+    fridgeItemId: 1,
+  },
+  {
+    quantity: 2.0,
+    userId: 1,
+    fridgeItemId: 2,
+  },
+  {
+    quantity: 3.0,
+    userId: 1,
+    fridgeItemId: 3,
+  },
+  {
+    quantity: 3.0,
+    userId: 1,
+    fridgeItemId: 4,
+  },
+  {
+    quantity: 3.0,
+    userId: 1,
+    fridgeItemId: 5,
+  },
+  {
+    quantity: 2.0,
+    userId: 1,
+    fridgeItemId: 6,
+  },
+  {
+    quantity: 2.0,
+    userId: 1,
+    fridgeItemId: 7,
+  },
+  {
+    quantity: 2.0,
+    userId: 2,
+    fridgeItemId: 1,
+  },
+  {
+    quantity: 2.0,
+    userId: 2,
+    fridgeItemId: 2,
+  },
+  {
+    quantity: 2.0,
+    userId: 2,
+    fridgeItemId: 3,
+  },
+  {
+    quantity: 1.0,
+    userId: 2,
+    fridgeItemId: 4,
+  },
+  {
+    quantity: 1.0,
+    userId: 2,
+    fridgeItemId: 5,
+  },
+  {
+    quantity: 1.0,
+    userId: 2,
+    fridgeItemId: 6,
+  },
+  {
+    quantity: 1.0,
+    userId: 2,
+    fridgeItemId: 7,
+  },
+  {
+    quantity: 2.0,
+    userId: 3,
+    fridgeItemId: 1,
+  },
+  {
+    quantity: 2.0,
+    userId: 3,
+    fridgeItemId: 2,
+  },
+  {
+    quantity: 2.0,
+    userId: 3,
+    fridgeItemId: 3,
+  },
+  {
+    quantity: 1.0,
+    userId: 3,
+    fridgeItemId: 4,
+  },
+  {
+    quantity: 1.0,
+    userId: 3,
+    fridgeItemId: 5,
+  },
+  {
+    quantity: 1.0,
+    userId: 3,
+    fridgeItemId: 6,
+  },
+  {
+    quantity: 1.0,
+    userId: 3,
+    fridgeItemId: 7,
+  },
+  {
+    quantity: 2.0,
+    userId: 4,
+    fridgeItemId: 1,
+  },
+  {
+    quantity: 2.0,
+    userId: 4,
+    fridgeItemId: 2,
+  },
+  {
+    quantity: 2.0,
+    userId: 4,
+    fridgeItemId: 3,
+  },
+  {
+    quantity: 1.0,
+    userId: 4,
+    fridgeItemId: 4,
+  },
+  {
+    quantity: 1.0,
+    userId: 4,
+    fridgeItemId: 5,
+  },
+  {
+    quantity: 1.0,
+    userId: 4,
+    fridgeItemId: 6,
+  },
+  {
+    quantity: 1.0,
+    userId: 4,
+    fridgeItemId: 7,
+  },
+  {
+    quantity: 2.0,
+    userId: 5,
+    fridgeItemId: 1,
+  },
+  {
+    quantity: 2.0,
+    userId: 5,
+    fridgeItemId: 2,
+  },
+  {
+    quantity: 2.0,
+    userId: 5,
+    fridgeItemId: 3,
+  },
+  {
+    quantity: 1.0,
+    userId: 5,
+    fridgeItemId: 4,
+  },
+  {
+    quantity: 1.0,
+    userId: 5,
+    fridgeItemId: 5,
+  },
+  {
+    quantity: 1.0,
+    userId: 5,
+    fridgeItemId: 6,
+  },
+  {
+    quantity: 1.0,
+    userId: 5,
+    fridgeItemId: 7,
+  },
+];
+const seed = () =>
+  Promise.all(users.map(user =>
+    User.create(user)))
+    .then(() =>
+      Promise.all(fridgeItems.map(fridgeItem =>
+        FridgeItems.create(fridgeItem))))
+    .then(() =>
+      Promise.all(fridges.map(fridge =>
+        Fridge.create(fridge))));
+const main = () => {
+  console.log('Syncing db...');
+  db.sync({ force: true })
+    .then(() => {
+      console.log('Seeding databse...');
+      return seed();
+    })
+    .catch((err) => {
+      console.log('Error while seeding');
+      console.log(err.stack);
+    })
+    .then(() => {
+      db.close();
+      return null;
+    });
+};
+
+main();
