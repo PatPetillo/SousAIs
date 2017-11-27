cb71368b6eabbdec9e7a70c2b2729639be7fc9ef
Yuchen Chen <yuchchen1314@gmail.com>
2017-11-27 15:40:39 -0500

    singleitem component now shows possible recipe

--- client/components/SingleItem.jsx
+++ client/components/SingleItem.jsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { getSingleItemRecipeToStore } from '../store/recipe';
 
-const singleProduct = 'SingleProduct';
 const recipes = ['cook', 'fried', 'grill'];
 const nutritionList = ['vitamin A', 'potassium', 'magnessium'];
 
-function SingleItem(props) {
-  const singleItem = props.fridge.filter(item => item.id === +props.match.params.id);
-  return (
-    <div className="singleItem">
-      <p>{singleItem.length && singleItem[0].name.toUpperCase()}</p>
-      <img src={singleItem.length && singleItem[0].image} alt="Yuchen's fault" />
-      <div>
-        {recipes.length && recipes.map(recipe => <div key={recipe}>{recipe}</div>)}
+class SingleItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    this.props.getSingleItemRecipeToStore(this.props.match.params.id)
+  }
+  render() {
+    const singleItem = this.props.fridge.filter(item => item.id === +this.props.match.params.id);
+    const recipes = this.props.recipe.singleItemRecipes
+    return (
+      <div className="singleItem">
+        <p>{singleItem.length && singleItem[0].name.toUpperCase()}</p>
+        <img src={singleItem.length && singleItem[0].image} alt="Yuchen's fault" />
+        <div>
+          {recipes.length && recipes.map(recipe => <div key={recipe.image}><NavLink to={`/${recipe.name.split(' ').join('')}`}>{recipe.name}</NavLink></div>)}
+        </div>
+        <div>
+          {nutritionList.length && nutritionList.map(nutrition => <div key={nutrition}>{nutrition}</div>)}
+        </div>
       </div>
-      <div>
-        {nutritionList.length && nutritionList.map(nutrition => <div key={nutrition}>{nutrition}</div>)}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 
-const mapState = ({ fridge }) => ({ fridge });
-const mapDispatch = {};
+
+const mapState = ({ fridge, recipe }) => ({ fridge, recipe });
+const mapDispatch = { getSingleItemRecipeToStore };
 export default connect(mapState, mapDispatch)(SingleItem);
 
