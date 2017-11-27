8c96b29ceb66c7c3729dc56c3f2bce05e4e9f04b
Bderiel <bderiel@gmail.com>
2017-11-27 17:18:31 -0500

    hardcode

--- server/api/recipe.js
+++ server/api/recipe.js
@@ -9,48 +9,6 @@ module.exports = router;
 
 
 router.get('/', (req, res, next) => {
-    let user;
-    User.findById(1)
-        .then((founduser) => {
-            user = founduser;
-            return user.getFridgeItems();
-        })
-        .then((foundItems) => {
-            const ingredients = foundItems.map(x => x.name);
-            return axios.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=${ingredients.join('%2C')}&limitLicense=false&number=5&ranking=1`, {
-                    headers: {
-                      'X-Mashape-Key': key,
-                        Accept: 'application/json',
-                    },
-                })
-                .then((apiRes) => {
-                    const rcpIds = apiRes.data.map(recipe => recipe.id).join('%2C');
-                    return axios.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/informationBulk?ids=${rcpIds}&includeNutrition=true`, {
-                        headers: {
-                          'X-Mashape-Key': key,
-                            Accept: 'application/json',
-                        },
-                    });
-                })
-                .then((rcps) => {
-                    const arrToUpdate = [];
-                    const meals = rcps.data.filter(recipes => !!recipes.analyzedInstructions.length);
-                    const info = meals.map(meal => ({
-                        name: meal.title,
-                        steps: meal.analyzedInstructions[0].steps.map(el => el.step).join('$$'),
-                        userId: req.session.passport.user,
-                        calories: meal.nutrition.nutrients[0].amount + ' ' + meal.nutrition.nutrients[0].unit,
-                        fat: meal.nutrition.nutrients[1].amount + ' ' + meal.nutrition.nutrients[1].unit,
-                        carbohydrates: meal.nutrition.nutrients[3].amount + ' ' + meal.nutrition.nutrients[3].unit,
-                        sugar: meal.nutrition.nutrients[4].amount + ' ' + meal.nutrition.nutrients[4].unit,
-                        sodium: meal.nutrition.nutrients[6].amount + ' ' + meal.nutrition.nutrients[6].unit,
-                        image: meal.image,
-                    }));
-                    info.forEach(el => arrToUpdate.push(Recipe.create(el)));
-                    return Promise.all(arrToUpdate)
-                        .then((recipes) => {
-                            recipes.forEach(toSetR => user.addRecipe(toSetR.id));
-                            //res.json(recipes);
                           res.json([
                             {
                               "id": 37,
@@ -93,9 +51,6 @@ router.get('/', (req, res, next) => {
                             }
                           ])
                         });
-                });
-        })
-});
 
 router.post('/', (req, res, next) => {
     User.findById(1)
