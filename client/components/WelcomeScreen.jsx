a7ca118d79e913426ccb8a734469e0dff8ba6b2a
Yuchen Chen <yuchchen1314@gmail.com>
2017-11-29 15:32:35 -0500

    removed commet

--- client/components/WelcomeScreen.jsx
+++ client/components/WelcomeScreen.jsx
@@ -6,62 +6,6 @@ const descriptions = [
   'Provides you with the nutritional info you need to make well informed choices',
   'Takes the guess work out of cooking',
 ];
-// export default class WelcomeScreen extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       recipeName: '',
-//     };
-//     this.inputChange = this.inputChange.bind(this);
-//     this.handleSearch = this.handleSearch.bind(this);
-//   }
-
-//   inputChange(e) {
-//     this.setState({
-//       recipeName: e.target.value,
-//     });
-//   }
-//   handleSearch(e){
-//     console.log(this.state)
-//   }
-//   render() {
-//     return (
-//       <div className="welcome-page">
-//         <div className="py-5 text-center banner">
-//           <div className="container py-5">
-//             <div className="row">
-//               <div className="col-md-12 text-white">
-//                 <h1 className="display-3 mb-4">Sous with <span className="red-words">Alexa</span></h1>
-//                 <p className="lead mb-5" />
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         <div>
-//           <input name="recipeName" onChange={this.inputChange} />
-//           <button onClick={this.handleSearch}>Search</button>
-//         </div>
-//         <div className="motto bg-secondary"> <h1> Cooking - reinvented </h1> </div>
-//         <div className="carousel-info">
-//           <div className="carousel-container"> <WelcomeScreenCarousel /> </div>
-//           <div className="how-it-works bg-primary">
-//             <div className="all-descriptions">
-//               <h1 className="description-head"> Sous is your personal kitchen assistant </h1>
-//               <div className="descriptions">
-//                 {
-//                 descriptions.map(description => (
-//                   <p className="lead" key={description}> {description} </p>
-//                 ))
-//               }
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 
 const WelcomeScreen = () => (
   <div className="welcome-page">
