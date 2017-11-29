45ff53aa55da089c4ddbd3f55d793cc3eed8d866
Yuchen Chen <yuchchen1314@gmail.com>
2017-11-29 15:20:10 -0500

    created search recipe

---
+++ client/components/FoundRecipe.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FoundRecipe = () => {
+  console.log('foundrecipe');
+  return (<div>Found Recipeasdasdasasdasdadasdasdadasd</div>);
+};
+
+export default FoundRecipe;
+
