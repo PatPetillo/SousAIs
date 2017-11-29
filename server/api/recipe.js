ca19a42407f5ddd5ad9fccc477c319aac1e3c622
Wale Jegs <wale.a.jegede@gmail.com>
2017-11-29 12:25:00 -0500

    fix replaced route

--- server/api/recipe.js
+++ server/api/recipe.js
@@ -169,7 +169,57 @@ router.get('/startover', (req, res, next) => {
         }))
 });
 // Get possible recipes only base on  one item.
-
+router.get('/alexa', (req, res, next) => { // test only most merge
+    let user;
+    User.findById(1)
+        .then((founduser) => {
+            user = founduser;
+            return user.getFridgeItems();
+        })
+        .then((foundItems) => {
+            const ingredients = foundItems.map(x => x.name);
+            return axios.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=${ingredients.join('%2C')}&limitLicense=false&number=5&ranking=2`, {
+                    headers: {
+                        'X-Mashape-Key': process.env.SPOONACULAR_ID,
+                        Accept: 'application/json',
+                    },
+                })
+                .then((apiRes) => {
+                    const rcpIds = apiRes.data.map(recipe => recipe.id).join('%2C');
+                    return axios.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/informationBulk?ids=${rcpIds}&includeNutrition=true`, {
+                        headers: {
+                            'X-Mashape-Key': process.env.SPOONACULAR_ID,
+                            Accept: 'application/json',
+                        },
+                    });
+                })
+                .then((rcps) => {
+                    const arrToUpdate = [];
+                    const meals = rcps.data.filter(recipes => !!recipes.analyzedInstructions.length);
+                    const info = meals.map(meal => ({
+                        name: meal.title,
+                        steps: meal.analyzedInstructions[0].steps.map(el => el.step).join('$$'),
+                        userId: 1,
+                        calories: `${meal.nutrition.nutrients[0].amount} ${meal.nutrition.nutrients[0].unit}`,
+                        fat: `${meal.nutrition.nutrients[1].amount} ${meal.nutrition.nutrients[1].unit}`,
+                        carbohydrates: `${meal.nutrition.nutrients[3].amount} ${meal.nutrition.nutrients[3].unit}`,
+                        sugar: `${meal.nutrition.nutrients[4].amount} ${meal.nutrition.nutrients[4].unit}`,
+                        sodium: `${meal.nutrition.nutrients[6].amount} ${meal.nutrition.nutrients[6].unit}`,
+                        image: meal.image,
+                    }));
+                    info.forEach(el => arrToUpdate.push(Recipe.create(el)));
+                    return Promise.all(arrToUpdate)
+                        .then((recipes) => {
+                            recipes.forEach(toSetR => user.addRecipe(toSetR.id));
+                            user.update({
+                                cr: recipes[0].id,
+                                crSteps: recipes[0].steps,
+                            });
+                            res.json(recipes);
+                        });
+                });
+        });
+});
 
 // SINGLE ITEM RECIPES ROUTE FOR ALEXA
 router.get('/alexa/:food', (req, res, next) => {
