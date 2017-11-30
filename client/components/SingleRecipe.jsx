7398082a4c4431812b8b0539606c7b33b29b7f3d
Devon JohnBaptiste <devondevon@outlook.com>
2017-11-30 13:00:37 -0500

    fix(rendering): displays text when fridge is empty. displays text on recipes when fridge is empty. positioning changes

--- client/components/SingleRecipe.jsx
+++ client/components/SingleRecipe.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import ReactLoading from 'react-loading';
 
 const SingleRecipe = (props) => {
   const recipe = props.recipe.recipes.filter(singlerecipe => singlerecipe.name.split(' ').join('') === props.match.params.recipename)[0];
-  console.log(recipe);
   return (
     <div className="page-content">
       { recipe ?
-        <div>
+        <div className="single-recipe-page">
           <h2> {recipe.name} </h2>
           <h5>Serves: {recipe.servings}</h5>
           <div key={recipe.name}>
@@ -18,24 +18,9 @@ const SingleRecipe = (props) => {
               <li className="spoonacular-rating">spoonacular rating: {recipe.spoonacularScore}</li>
             </ul>
           </div>
-          <img className="recipe-image" src={recipe.image} alt={recipe.name} />
-          <h2>Directions</h2>
-          <ol className="single-recipe-directions">
-            {
-          recipe.steps.split('$$').map(sentence => (
-            (sentence.length) ? <li key={sentence}> {sentence}</li> : null))
-            }
-          </ol>
+
           <div className="single-recipe-footer">
-            <div className="single-recipe-footer-ingredients">
-              <h2>Ingredients</h2>
-              <div>
-                {
-                  recipe.ingredientAmount.split('$$').map(amount => (
-                    (amount.length) ? <div key={amount}> {amount}</div> : null))
-                }
-              </div>
-            </div>
+            <img className="recipe-image" src={recipe.image} alt={recipe.name} />
             <div className="single-recipe-footer-nutrition">
               <h2>Nutritional Value</h2>
               <div>(per serving)</div>
@@ -48,9 +33,35 @@ const SingleRecipe = (props) => {
               <div>protein: {recipe.protein}</div>
             </div>
           </div>
+
+          <div className="single-recipe-details">
+            <div className="single-recipe-footer-ingredients">
+              <h2>Ingredients</h2>
+              <div>
+                {
+                  recipe.ingredientAmount.split('$$').map(amount => (
+                    (amount.length) ? <div key={amount}> {amount}</div> : null))
+                }
+              </div>
+            </div>
+            <div className="single-recipe-directions">
+              <h2>Directions</h2>
+              <ol className="single-recipe-steps">
+                {
+              recipe.steps.split('$$').map(sentence => (
+                (sentence.length) ? <li key={sentence}> {sentence}</li> : null))
+                }
+              </ol>
+            </div>
+          </div>
         </div>
     :
-        <div>go to recipes page to load a single recipe</div>
+        <div>
+          <div className="react-loading" >
+            <ReactLoading type="spinningBubbles" color="#7df096" height="100px" width="100px" />
+          </div>
+          <div>loading...</div>
+        </div>
     }
     </div>
   );
@@ -62,4 +73,5 @@ export default connect(mapState)(SingleRecipe);
 
 SingleRecipe.propTypes = {
   recipe: PropTypes.objectOf(PropTypes.any).isRequired,
+  match: PropTypes.objectOf(PropTypes.any).isRequired,
 };
