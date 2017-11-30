fc920a8d5023c7c9206aa4deb27ededc24fc904a
Devon JohnBaptiste <devondevon@outlook.com>
2017-11-30 13:00:37 -0500

    fix(rendering): displays text when fridge is empty. displays text on recipes when fridge is empty. positioning changes

--- client/components/AllRecipes.jsx
+++ client/components/AllRecipes.jsx
@@ -21,27 +21,33 @@ const AllRecipes = (props) => {
     <div className="page-content">
       <h1>{`${user.name}'s Recipes`}</h1>
       {
-        recipes.length ? recipes.sort((a, b) => a.missedIngredientCount > b.missedIngredientCount).map(oneRecipe => (
-          <div key={oneRecipe.id}>
-            <h2>{oneRecipe.name}</h2>
-            <p>Missing IngredientCount: {oneRecipe.missedIngredientCount}</p>
-            <div>
-              <img className="oneRecipeImage" src={oneRecipe.image} alt={oneRecipe.name} />
-            </div>
-            <NavLink to={`/singleRecipe/${oneRecipe.name.split(' ').join('')}`}>
-              <div className="btn btn-primary my-3">Directions</div>
-            </NavLink>
-            <NavLink to="#">
-              {savedRecipe.includes(oneRecipe) ?
-                <button className="btn disabled-btn" disabled>
-                  Saved
-                </button> :
-                <button className="btn btn-primary my-3" onClick={(e) => { disableButton(e); saveRecipeToStore(oneRecipe); }}>
-                    Save This Recipe
-                </button>
+        recipes.length
+        ? recipes.sort((a, b) => a.missedIngredientCount > b.missedIngredientCount)
+          .map(oneRecipe => (
+            <div key={oneRecipe.name}>
+              <h2>{oneRecipe.name}</h2>
+              {(oneRecipe.name === 'You have no items in your fridge.') ? null :
+              <div>
+                <p>Missing IngredientCount: {oneRecipe.missedIngredientCount}</p>
+                <div>
+                  <img className="oneRecipeImage" src={oneRecipe.image} alt={oneRecipe.name} />
+                </div>
+                <NavLink to={`/singleRecipe/${oneRecipe.name.split(' ').join('')}`}>
+                  <div className="btn btn-primary my-3">Directions</div>
+                </NavLink>
+                <NavLink to="#">
+                  {savedRecipe.includes(oneRecipe) ?
+                    <button className="btn disabled-btn" disabled>
+                      Saved
+                    </button> :
+                    <button className="btn btn-primary my-3" onClick={(e) => { disableButton(e); saveRecipeToStore(oneRecipe); }}>
+                        Save This Recipe
+                    </button>
+                  }
+                </NavLink>
+              </div>
               }
-            </NavLink>
-          </div>))
+            </div>))
         : (
           <div>
             <div className="react-loading" >
